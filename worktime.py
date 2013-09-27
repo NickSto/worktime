39147e7b9c234e535719b0a74d5ca0772babbcf1
me <nmapsy@gmail.com>
2013-09-27 00:07:25 -0400

    Original python and bash scripts

---
+++ worktime.py
@@ -0,0 +1,109 @@
+#!/usr/bin/python
+import os, sys
+
+MODES = ['w','p','n','s']
+LOG_FILE_REL    = '.mymisc/worklog.txt'
+STATUS_FILE_REL = '.mymisc/workstatus.txt'
+CMD_CLEAR  = 'clear'
+CMD_ADJUST = 'adjust'
+
+log_file    = os.path.expanduser('~')+os.sep+LOG_FILE_REL
+status_file = os.path.expanduser('~')+os.sep+STATUS_FILE_REL
+
+def main():
+
+  if len(sys.argv) == 1 or '-h' in sys.argv[1][0:3]:
+    script_name = os.path.basename(sys.argv[0])
+    print """USAGE:
+  $ """+script_name+""" [mode]
+or
+  $ """+script_name+""" [command] [arguments]
+Where [mode] is a single letter (one of """+', '.join(MODES)+""")
+and [command] is one of the following:
+  clear:
+Clears the log; restarts at 0 for all modes.
+  adjust:
+Add or subtract times from the recorded log."""
+    sys.exit(0)
+  else:
+    arg1 = sys.argv[1]
+    if arg1 in MODES:
+      switch_mode(arg1)
+    else:
+      run_command(arg1)
+
+
+def switch_mode(mode):
+  """Mark the time spent in the last mode and mark this new one"""
+  fail("Oops, this script doesn't do the logging yet!")
+  checkfiles(log_file, status_file)
+
+
+def run_command(command):
+  """Execute a special command"""
+  checkfiles(log_file, status_file)
+
+  if command == CMD_CLEAR:
+    clear()
+  elif command == CMD_ADJUST:
+    adjustments = sys.argv[2:]
+    adjust(adjustments)
+  else:
+    fail('Error: Command "'+command+'" not recognized!')
+
+
+def clear():
+  """Clears the log, resetting all times to 0"""
+  fail("Oops, this script can't do 'clear' yet!")
+
+
+def adjust(adjustments):
+  """Change the accumulated times in the work log"""
+  
+  times = readlog(log_file)
+
+  for adjustment in adjustments:
+    if len(adjustment) < 3:
+      fail("Error: adjustment syntax incorrect in "+adjustment)
+    mode = adjustment[0:1]
+    try:
+      amount = int(adjustment[1:])
+    except ValueError, e:
+      fail("Error: adjustment syntax incorrect in "+adjustment)
+    if mode not in MODES:
+      fail("Error: invalid mode given in adjustment command "+adjustment)
+
+    times[mode] = times.get(mode, 0) + 60 *amount
+
+  writelog(log_file, times)
+
+
+def checkfiles(log_file, status_file):
+  """Make sure the two files exist"""
+
+
+def readlog(log_file):
+  """Read log file into a dict"""
+  times = {}
+  with open(log_file, 'r') as lines:
+    for line in lines:
+      [mode, seconds] = line.rstrip("\r\n").split("\t")
+      times[mode] = int(seconds)
+  return times
+
+
+def writelog(log_file, times):
+  """Replace existing log file with new version"""
+  with open(log_file, 'w') as log_fh:
+    for mode in times:
+      log_fh.write(mode+"\t"+str(times.get(mode, 0))+"\n")
+
+
+def fail(message):
+  """Quick way of writing the below two lines"""
+  sys.stderr.write(message+"\n")
+  sys.exit(1)
+
+
+if __name__ == "__main__":
+  main()
---
+++ worktime.sh
@@ -0,0 +1,150 @@
+#!/bin/bash
+set -ue
+
+WORK='w'
+PLAY='p'
+NEUT='n'
+STOP='s'
+CMND='0'
+CLEAR_CMND='clear'
+STATUS_CMND='status'
+ADJUST_CMND='adjust'
+STATUS_FILE='/home/me/.mymisc/workstatus.txt'
+LOG_FILE='/home/me/.mymisc/worklog.txt'
+LOG_TEMP='/home/me/.mymisc/worktemp.txt'
+NOTIFY_TEMP='/home/me/.mymisc/notifytemp.txt'
+
+if [[ ! "$@" ]]; then
+  echo "USAGE:
+  $ $0 [type]
+Where [type] is:
+$WORK    work
+$PLAY    play
+$NEUT    neutral
+$STOP    stop"
+  exit
+else
+  type="$1"
+  if [ ${#type} -gt 1 ]; then
+    echo "Error: first argument not a recognized type" 1>&2
+    exit 1
+  fi
+fi
+
+# convert seconds to minutes and hours
+to_time () {
+  sec_total="$1"
+  min=$(echo "$sec_total/60" | bc)
+  sec=$((sec_total - (min * 60)))
+  if [ $min -lt 60 ]; then
+    echo "$min"
+  else
+    min_total=$min
+    hr=$(echo "$min_total/60" | bc)
+    min=$((min_total - (hr * 60)))
+    if [ $min -lt 10 ]; then min='0'$min; fi
+    echo "$hr:$min"
+  fi
+}
+
+# If first argument is $CMND, execute special command
+if [ "$type" == "$CMND" ]; then
+  if [ $# -lt 2 ]; then
+    echo "Error: must provide a command argument after $CMND" 1>&2
+    exit 1
+  fi
+  command="$2"
+
+  # reset times log
+  if [ "$command" == "$CLEAR_CMND" ]; then
+    if [ -f "$LOG_FILE" ]; then rm "$LOG_FILE"; fi
+    echo -ne "$STOP\t"$(date +%s) > "$STATUS_FILE"
+    notify-send "log cleared"
+  fi
+
+  # display status without changing state
+  if [ "$command" == "$STATUS_CMND" ]; then
+    if [[ -f "$STATUS_FILE" && -f "$LOG_FILE" ]]; then
+      now=$(date +%s)
+      oldtype=$(cut -f 1 "$STATUS_FILE")
+      oldtime=$(cut -f 2 "$STATUS_FILE")
+      elapsed=$((now-oldtime))
+      elapsed_time=$(to_time $elapsed)
+      echo -n > "$NOTIFY_TEMP"
+      cat "$LOG_FILE" | while read line; do
+        logtype=$(echo "$line" | cut -f 1)
+        logtime=$(echo "$line" | cut -f 2)
+        echo -n "$logtype\t"$(to_time $logtime)"\n" >> "$NOTIFY_TEMP"
+      done
+      notify_string=$(cat "$NOTIFY_TEMP")
+      notify-send "Status:  $oldtype     $elapsed_time" \
+        "$(echo -ne "$notify_string")"
+    else
+      notify-send "Error" "Not started or missing files"
+    fi
+  fi
+
+  if [ "$command" == "$ADJUST_CMND" ]; then
+    for arg in $@; do
+      if [[ "$arg" == "$CMND" || "$arg" == "$ADJUST_CMND" ]]; then continue; fi
+      adj_type=${arg:0:1}
+      adj_op=${arg:1:1}
+      adj_amt=${arg:2}
+    done
+  fi
+
+  exit
+fi
+
+
+# Get elapsed time since last invocation
+# from $STATUS_FILE
+now=$(date +%s)
+if [ -f "$STATUS_FILE" ]; then
+  oldtype=$(cut -f 1 "$STATUS_FILE")
+  oldtime=$(cut -f 2 "$STATUS_FILE")
+
+  elapsed=$((now-oldtime))
+  to_time $elapsed
+fi
+# Replace $STATUS_FILE
+echo -ne "$type\t$now" > "$STATUS_FILE"
+
+
+# Read $LOG_FILE, update totals, write new file, send notification to screen
+work_time=0
+play_time=0
+neut_time=0
+if [ -f "$LOG_FILE" ]; then
+  cat "$LOG_FILE" | while read line; do
+    logtype=$(echo "$line" | cut -f 1)
+    logtime=$(echo "$line" | cut -f 2)
+    if [ "$logtype" == "$oldtype" ]; then
+      logtime=$((logtime + elapsed))
+      echo "adjusted logtime to $logtime"
+    fi
+
+    if [ "$logtype" == "$WORK" ]; then
+      work_time=$logtime
+      echo -e "$WORK\t$work_time" > "$LOG_TEMP"
+      echo -n "$WORK\t"$(to_time $work_time)"\n" > "$NOTIFY_TEMP"
+    fi
+    if [ "$logtype" == "$PLAY" ]; then
+      play_time=$logtime
+      echo -e "$PLAY\t$play_time" >> "$LOG_TEMP"
+      echo -n "$PLAY\t"$(to_time $play_time)"\n" >> "$NOTIFY_TEMP"
+    fi
+    if [ "$logtype" == "$NEUT" ]; then
+      neut_time=$logtime
+      echo -e "$NEUT\t$neut_time" >> "$LOG_TEMP"
+      echo -n "$NEUT\t"$(to_time $neut_time)"\n" >> "$NOTIFY_TEMP"
+    fi
+  done
+else
+  echo -e "$WORK\t0\n$PLAY\t0\n$NEUT\t0" > "$LOG_TEMP"
+  echo -e "$WORK\t0\n$PLAY\t0\n$NEUT\t0" > "$NOTIFY_TEMP"
+fi
+
+mv "$LOG_TEMP" "$LOG_FILE"
+notify_string=$(cat "$NOTIFY_TEMP")
+notify-send "$type" "$(echo -ne "$notify_string")"
