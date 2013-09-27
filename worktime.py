98cd0f9d72455724ee91396f857f82297b95c45a
me <nmapsy@gmail.com>
2013-09-27 04:49:32 -0400

    Finished everything but notify-send component. Now testing.

--- worktime.py
+++ worktime.py
@@ -15,6 +15,7 @@ import sys
 import time
 
 MODES = ['w','p','n','s']
+HIDDEN = ['s']
 LOG_FILE_REL    = '.mymisc/worklog.txt'
 STATUS_FILE_REL = '.mymisc/workstatus.txt'
 CMD_CLEAR  = 'clear'
@@ -51,8 +52,6 @@ Add or subtract times from the recorded log."""
 # seems to be working
 def switch_mode(mode):
   """Mark the time spent in the last mode and mark this new one"""
-  sys.stderr.write("Oops, this script doesn't do the logging yet!\n")
-  checkfiles(log_file, status_file)
 
   if mode not in MODES:
     fail("Error: Unrecognized mode "+mode)
@@ -60,37 +59,38 @@ def switch_mode(mode):
   now = int(time.time())
   times = readlog(log_file)
   status = readlog(status_file)
-  if len(status) != 1:
+  if len(status) > 1:
     fail("Error: Status file "+status_file+" should have 1 entry. Currently "+
       "has "+str(len(status)))
+  elif len(status) < 1:
+    status = {mode:now}
+
   old_mode = status.keys()[0]
   elapsed = now - status[old_mode]
 
   times[old_mode] = elapsed + times.get(old_mode, 0)
   status = {mode:now}
-  #TODO: also print new times to notify-send
 
   writelog(log_file, times)
   writelog(status_file, status)
+  print_times(mode)
 
 
 def run_command(command, args):
   """Execute a special command"""
-  checkfiles(log_file, status_file)
 
   if command == CMD_CLEAR:
     clear()
   elif command == CMD_ADJUST:
     adjust(args)
   elif command == CMD_STATUS:
-    print_status()
+    print_times(status_str())
   else:
     fail('Error: Command "'+command+'" not recognized!')
 
 
 def clear():
   """Clears the log"""
-  sys.stdout.write("Oops, this script can't do 'clear' yet!\n")
   # don't print 0's. just blank it, and the 0's will be inferred when needed
   writelog(log_file, {})
   writelog(status_file, {}) # not certain about this one yet
@@ -105,13 +105,12 @@ def adjust(adjustments):
 
   for adjustment in adjustments:
 
-    try:
+    if '+' in adjustment:
       index = adjustment.index("+")
-    except ValueError, e:
-      try:
-        index = adjustment.index("-")
-      except ValueError, e:
-        fail("Error: adjustment syntax incorrect in "+adjustment)
+    elif '-' in adjustment:
+      index = adjustment.index("-")
+    else:
+      fail("Error: adjustment syntax incorrect in "+adjustment)
 
     mode = adjustment[:index]
     try:
@@ -122,19 +121,22 @@ def adjust(adjustments):
       fail("Error: invalid mode given in adjustment command "+adjustment)
 
     times[mode] = times.get(mode, 0) + 60 * amount
+    if times[mode] < 0:
+      times[mode] = 0
 
   writelog(log_file, times)
 
+  print_times()
+
 
 ##### Basic I/O #####
-
-def checkfiles(log_file, status_file):
-  """Make sure the two files exist"""
 
 
 def readlog(log_file):
   """Read log file into a dict (works for status file too)"""
   times = {}
+  if not os.path.exists(log_file):
+    return times
   with open(log_file, 'r') as lines:
     for line in lines:
       (mode, seconds) = line.rstrip("\r\n").split("\t")
@@ -142,41 +144,60 @@ def readlog(log_file):
   return times
 
 
-"""DEBUG VERSION"""
-def writelog(log_file, times):
+def writelog_debug(log_file, times):
   """Replace existing log file with new version (works for status file too)"""
   # with open(log_file, 'w') as log_fh:
   for mode in times:
     sys.stdout.write(mode+"\t"+str(times.get(mode, 0))+"\n")
       # log_fh.write(mode+"\t"+str(times.get(mode, 0))+"\n")
 
-def writelog_real(log_file, times):
+def writelog(log_file, times):
   """Replace existing log file with new version (works for status file too)"""
   with open(log_file, 'w') as log_fh:
     for mode in times:
       log_fh.write(mode+"\t"+str(times.get(mode, 0))+"\n")
 
 
-def print_status():
-  times = readlog(log_file)
-  for mode in times:
-    print mode+"\t"+str(times[mode])
+def status_str():
+  status = readlog(status_file)
+  if status:
+    mode = status.keys()[0]
+    elapsed = int(time.time()) - status[mode]
+    return mode+"\t"+datestring(elapsed)
+  return ""
 
-def print_status_real():
+
+def print_times(message=""):
+  if message:
+    print "    "+message
+  times = readlog(log_file)
+  # get union of builtin modes and ones in file (each mode only once)
+  modes = list(set(times.keys()) | set(MODES))
+  for mode in modes:
+    if mode not in HIDDEN:
+      print mode+"\t"+datestring(times.get(mode, 0))
+
+def print_times_real(message=""):
   times = readlog(log_file)
   for mode in times:
     #TODO: print to notify-send
-    # don't show 's' time
     print mode+"\t"+str(times[mode])
 
+
+def datestring(sec_total):
+  """Convert time in seconds to HH:MM:SS string"""
+  min_total = sec_total / 60
+  hrs = min_total / 60
+  min = min_total % 60
+  if hrs:
+    return "%d:%02d" % (hrs, min)
+  else:
+    return str(min)
 
 def fail(message):
   """Quick way of writing the below two lines"""
   sys.stderr.write(message+"\n")
   sys.exit(1)
-
-
-##### Basic stuff #####
 
 
 
