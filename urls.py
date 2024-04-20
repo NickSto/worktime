0426a08e45f1de1cf2a87e93094b000492b8eeac
Nick <git@mailship.org>
2024-04-20 00:12:04 -0400

    urls.py: Update for Django 4.x compatibility.

--- urls.py
+++ urls.py
@@ -1,15 +1,15 @@
-from django.conf.urls import url
+from django.urls import re_path
 
 from . import views
 
 app_name = 'worktime'
 
 urlpatterns = [
-  url(r'^$', views.main, name='main'),
-  url(r'switch$', views.switch, name='switch'),
-  url(r'adjust$', views.adjust, name='adjust'),
-  url(r'switchera$', views.switchera, name='switchera'),
-  url(r'renamera$', views.renamera, name='renamera'),
-  url(r'clear$', views.clear, name='clear'),
-  url(r'settings$', views.settings, name='settings'),
+  re_path(r'^$', views.main, name='main'),
+  re_path(r'switch$', views.switch, name='switch'),
+  re_path(r'adjust$', views.adjust, name='adjust'),
+  re_path(r'switchera$', views.switchera, name='switchera'),
+  re_path(r'renamera$', views.renamera, name='renamera'),
+  re_path(r'clear$', views.clear, name='clear'),
+  re_path(r'settings$', views.settings, name='settings'),
 ]
